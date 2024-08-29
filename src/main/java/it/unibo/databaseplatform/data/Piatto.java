9bcd0cb445502e0bdf4c75856eed920f5e0b7283
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-08-29 14:43:46 +0200

    Initialized Piatto class

--- src/main/java/it/unibo/databaseplatform/data/Piatto.java
+++ src/main/java/it/unibo/databaseplatform/data/Piatto.java
@@ -1,4 +1,18 @@
 package it.unibo.databaseplatform.data;
 
 public class Piatto {
+
+    private final String codicePiatto;
+    private final String nomePiatto;
+    private final float prezzoPorzione;
+    private final int apportoCaloricoPorzione;
+    private final String codicePortata;
+
+    public Piatto(String codicePiatto, String nomePiatto, float prezzoPorzione, int apportoCaloricoPorzione, String codicePortata){
+        this.codicePiatto = codicePiatto;
+        this.nomePiatto = nomePiatto;
+        this.prezzoPorzione = prezzoPorzione;
+        this.apportoCaloricoPorzione = apportoCaloricoPorzione;
+        this.codicePortata = codicePortata;
+    }
 }
