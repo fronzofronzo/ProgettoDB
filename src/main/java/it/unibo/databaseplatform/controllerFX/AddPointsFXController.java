636759c659e4afa790e9291e2b3be377bf749212
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-09-02 14:46:03 +0200

    Various changes

--- src/main/java/it/unibo/databaseplatform/controllerFX/AddPointsFXController.java
+++ src/main/java/it/unibo/databaseplatform/controllerFX/AddPointsFXController.java
@@ -2,7 +2,10 @@ package it.unibo.databaseplatform.controllerFX;
 
 import it.unibo.databaseplatform.view.View;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class AddPointsFXController implements FXController{
 
@@ -11,6 +14,8 @@ public class AddPointsFXController implements FXController{
     private TextField cardField;
     @FXML
     private TextField pointsField;
+    @FXML
+    private Label textLabel;
 
     @Override
     public void setView(View view) {
@@ -19,7 +24,25 @@ public class AddPointsFXController implements FXController{
 
     @FXML
     public void addPoints() {
+        if(this.cardField.getText().isEmpty() || this.cardField.getText().isEmpty()) {
+            this.textLabel.setText("Campi vuoti: Operazione non valida !");
+        }
+        final var code = Integer.parseInt(this.cardField.getText());
+        final var points = Integer.parseInt(this.pointsField.getText());
+        if(this.view.getController().addPoints(code,points)) {
+            this.textLabel.setText("Aggiunti " + points + " punti alla carta " + code);
+        } else {
+            this.textLabel.setText("Errore! Controllare i campi inseriti ");
+        }
+    }
 
+    @FXML
+    public void backHome() {
+        try {
+            this.view.setScene("admin-view");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
