2eee83cc464698995f164224b7867e813dad09a5
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-09-09 12:19:58 +0200

    Modified view of the dish leaderboard

--- src/main/java/it/unibo/databaseplatform/controllerFX/MostOrderedDishesFXController.java
+++ src/main/java/it/unibo/databaseplatform/controllerFX/MostOrderedDishesFXController.java
@@ -3,6 +3,7 @@ package it.unibo.databaseplatform.controllerFX;
 import it.unibo.databaseplatform.view.View;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
 
@@ -12,25 +13,19 @@ public class MostOrderedDishesFXController implements FXController{
 
     private View view;
     @FXML
-    private GridPane leaderBoardGrid;
+    private ListView<String> dishesList;
 
     @Override
     public void setView(final View view) {
         this.view = view;
-        final var dishLabel = new Label("Piatto");
-        dishLabel.setPadding(new Insets(10));
-        leaderBoardGrid.add(dishLabel,0,0);
-        final var ordersLabel = new Label("Numero Ordini");
-        ordersLabel.setPadding(new Insets(10));
-        leaderBoardGrid.add(ordersLabel,1,0);
-        final var dishesList = this.view.getController().mostOrderedDishes();
-        for(int i = 0; i < dishesList.size(); i++) {
-            final var nameLabel = new Label(dishesList.get(i).getX());
-            nameLabel.setPadding(new Insets(10));
-            leaderBoardGrid.add(nameLabel, 0, i+1);
-            final var numberLabel = new Label(String.valueOf(dishesList.get(i).getY()));
-            numberLabel.setPadding(new Insets(10));
-            leaderBoardGrid.add(numberLabel, 1, i+1);
+        final var dishes = this.view.getController().mostOrderedDishes();
+        for(int i = 0; i < dishes.size(); i++) {
+            final var name = dishes.get(i).getX();
+            final var numOrders = String.valueOf(dishes.get(i).getY());
+            dishesList.getItems().add(
+                    String.valueOf(i+1) + ") Piatto: "
+                    + name + " - Numero ordini: "
+                    + numOrders);
         }
     }
 
